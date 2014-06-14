612012fba66fdcd7ade1304f38e13d40ea6bd565
tdubourg <tdubourg@users.noreply.github.com>
2014-06-14 02:04:31 +0200

    Please do not add modules, we are using different platforms :( + added changes to the server + cleaned up some code

--- frontend/server.js
+++ frontend/server.js
@@ -5,18 +5,19 @@ var http = require("http"),
     port = process.argv[2] || 8888;
 var qs = require('querystring');
 var sqlite3 = require('sqlite3');	
-var dbPath = "database.db";
+var dbPath = "../database.db";
 var db = new sqlite3.Database(dbPath);
 var externalrequest = require('request');
 var exec = require('child_process').exec;
 
 var test_learning = function (file_path, data_to_match, callback) {
-	exec("python ../backend/learning_test.py", function (err, stdout, stderr) {
-		if (err || stdout !== "0") {
-			console.log("Test for", file_path, "and data to match", data_to_match, "failed with error code", stdout)
+	exec("python ../backend/learning_test.py " + file_path + " " + data_to_match, function (err, stdout, stderr) {
+		if (err || stdout.replace("\n",'') !== '0') {
+			console.log("Test for", file_path, "and data to match=", data_to_match, "failed with error code", stdout)
 			callback(false)
+		} else {
+			callback(true)
 		}
-		callback(true)
 	})
 }
 
@@ -37,7 +38,7 @@ http.createServer(function(request, response) {
 	}
 	
 	request.addListener("end", function() {
-		if(request.url == "/GetSite"){
+		if(request.url == "/GetSite") {
 			response.writeHead(200, {"Content-Type": "text/plain"});
 			data = qs.parse(data)
 			//Form parameters
@@ -45,36 +46,37 @@ http.createServer(function(request, response) {
 			
 			//Get Webpage
 			console.log("Making request to " + data.url );
-				externalrequest(data.url, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var fs = require('fs');
+			externalrequest(data.url, function (error, req_resp, body) {
+				if (!error && req_resp.statusCode == 200) {
+					var fs = require('fs');
 
-				var fname = __dirname + "/tmp/"+ data.project
-				var stmt = db.prepare("INSERT INTO SEARCH(DATA_TO_MATCH, HTML_LEARNING_DATA, URL, NAME, PATTERN, CRAWL_LIMIT) VALUES(?,?,?,?,?,?)");
-				stmt.run(data.data, body, data.url, data.project, data.pattern, data.limit); 
-				stmt.finalize();
-				fs.writeFile(fname, body, function(err) {
-					if(err) {
-						console.log(err);
-					} else {
-				console.log("The file was saved!");
-				// Now, test it:
-				test_learning(fname, "test", function (result) {
-					if (result === true) {
-						response.write("\nThe learning test failed.")
-					} else {
-						response.write("\nThe learning test succeeded!")
-					}
-				})
-			}
-			}); 
-			//console.log(body) // Print the google web page.
-		} else {
-			console.log("error requesting webpage! " + response.statusCode + " " + data.UrlList );
-		}
-})
-			
-			response.end();
+					var fname = __dirname + "/tmp/"+ data.project
+					var stmt = db.prepare("INSERT INTO search(DATA_TO_MATCH, HTML_LEARNING_DATA, URL, NAME, PATTERN, CRAWL_LIMIT) VALUES(?,?,?,?,?,?)");
+					stmt.run(data.data, body, data.url, data.project, data.pattern, data.limit); 
+					stmt.finalize();
+					fs.writeFile(fname, body, function(err) {
+						if(err) {
+							console.log(err);
+						} else {
+							console.log("The file was saved!");
+							// Now, test it:
+							test_learning(fname, data.data, function (result) {
+								if (result === true) {
+									console.log("learning succeeded")
+									response.write("\nThe learning test succeeded!")
+								} else {
+									console.log("learning failed")
+									response.write("\nThe learning test failed.")
+								}
+								response.end();
+							})
+						}
+					}); 
+					//console.log(body) // Print the google web page.
+				} else {
+					console.log("error requesting webpage! " + response.statusCode + " " + data.UrlList );
+				}
+			})
 		}
 	});
 	
