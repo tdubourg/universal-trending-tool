908859a2b28afde97a23acbd1b1aec8d6178b028
tdubourg <tdubourg@users.noreply.github.com>
2014-06-14 04:41:02 +0200

    Might be useful at some point.

---
+++ backend/install_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo pip install scrapy
+sudo pip install scrapely
+sudo pip install slybot
+sudo pip install jswebkit
+sudo pip install webkit
+sudo pip install gtk2
+sudo pip install chardet
+
