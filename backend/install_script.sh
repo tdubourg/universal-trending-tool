e6f8a96b00e222422627d23192932419194fe51a
tdubourg <tdubourg@users.noreply.github.com>
2014-06-16 13:11:10 +0200

    Removing optional dependencies

--- backend/install_script.sh
+++ backend/install_script.sh
@@ -3,8 +3,8 @@
 sudo pip install scrapy
 sudo pip install scrapely
 sudo pip install slybot
-sudo pip install jswebkit
-sudo pip install webkit
-sudo pip install gtk2
+# sudo pip install jswebkit
+# sudo pip install webkit
+# sudo pip install gtk2
 sudo pip install chardet
 
